fe84aae4887650e25bc8bc6152b85b61a38b841c
Peter Jaeckel <peter@hapetech.de>
2020-08-28 22:34:03 +0200

    Successful\ Build

--- osrc/exec_message.c
+++ osrc/exec_message.c
@@ -4,6 +4,7 @@
 #include <malloc.h>
 
 void exec_message( CONTEXT ctx ) {
+    XXXX
     MESSAGE msg = malloc( sizeof( *msg ) );
     msg->obj = next_code_value(ctx);
     msg->cont = next_code_value(ctx);
