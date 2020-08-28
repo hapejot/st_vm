ece328166d9f077b54ab519d912019628e46aaa7
Peter Jaeckel <peter@hapetech.de>
2020-08-29 00:03:50 +0200

    Successful Build

--- osrc/exec_send.c
+++ osrc/exec_send.c
@@ -14,7 +14,13 @@ void exec_send( CONTEXT ctx ) {
     VALUE cls = value_obj_class( ctx->exec_msg->obj );
     CLS_MTH *m = lookup_method_and_class( cls, sel );
     if( m ) {
-        ctx->clr = value_closure_mk();
+        ctx->clr = value_closure_mk(  );
+        ctx->clr->tmp[0] = ctx->exec_msg->obj;
+        ctx->clr->tmp[1] = ctx->exec_msg->cont;
+        uint_t n = VALUE_IDX( ctx->exec_msg->argc );
+        for( uint_t i = 0; i < n; i++ ) {
+            ctx->clr->tmp[2 + i] = ctx->exec_msg->args[i];
+        }
         ctx->ref = m->code;
         ctx->code = value_code_ptr( ctx->ref );
     }
