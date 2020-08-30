9376697b9459b18e384e01e16c2fabe86e75babe
Peter Jaeckel <peter@hapetech.de>
2020-08-30 23:01:14 +0200

    Successful Build

--- osrc/exec_block.c
+++ osrc/exec_block.c
@@ -1,14 +1,9 @@
 #include "obj.h"
 #include "values.h"
+#include <stdbool.h>
+#include <assert.h>
+
 void exec_block( CONTEXT ctx ) {
-    ( void )ctx;
-    VALUE t = *ctx->code++;
-    switch ( VALUE_KIND( t ) ) {
-        case KIND_TREF:
-            ctx->clr->tmp[VALUE_IDX( t )] = value_code_start_block(  );
-            break;
-        default:
-            printf( "\nillegal value kind for target: %d", VALUE_KIND( t ) );
-            break;
-    }
+    (void)ctx;
+    assert(false);
 }
